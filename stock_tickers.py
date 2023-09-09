334a94ae46691729f546849684bd2b483efc4299
hschn58 <93406991+hschn58@users.noreply.github.com>
2023-09-09 17:17:17 -0500

    Update stock_tickers.py

--- stock_tickers.py
+++ stock_tickers.py
@@ -7,11 +7,6 @@ tableDf = tables[0]
 
 #filter for only what's needed
 data = pd.DataFrame(tableDf[0:150].drop(columns=['% Chg', 'Chg', 'Price', 'Symbol'], axis = 1))
-print(data)
-data.to_csv("NAME/PATH")
-# writer = pd.ExcelWriter(r"C:\Users\jim schnieders\Desktop\Dividends.xlsx", engine ='xlsxwriter')
-# data.to_excel(writer, sheet_name='Sheet1', index = False, header = True)
-# #set sheet width
-# writer.sheets['Sheet1'].set_column(0,2,25)
 
-# writer.save()
+#save
+data.to_csv(PATH)
